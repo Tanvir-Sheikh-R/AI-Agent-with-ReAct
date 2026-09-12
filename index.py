81f2f3e403caa96c280d716973ad8d1bd65904a0
Tanvir-Sheikh-R <tanvirsheikh0177@gmail.com>
2026-09-12 17:44:19 +0600

    leet practice

--- index.py
+++ index.py
@@ -1,1 +1,24 @@
-print('HI')
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) <= 1:
+            return len(s)
+        
+        l = 0
+        d = {}
+        res = 0
+        for r, val in enumerate(s):
+
+            if val in d and d[val] >= l:
+                l = d[val] + 1
+
+            d[val] = r
+
+            res = max(res, r - l + 1)
+        return res
+
+# s="abcabcbb"
+# s="pwwkew"
+s="dvdf"
+
+get = Solution()
+print(get.lengthOfLongestSubstring(s=s))
