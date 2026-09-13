3b7d30af200fbe4dbfa0249ec42b089ce5a592e0
Tanvir-Sheikh-R <tanvirsheikh0177@gmail.com>
2026-09-13 16:41:55 +0600

    refactor: remove unused Solution class and add environment variables

--- index.py
+++ index.py
@@ -1,24 +1,2 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) <= 1:
-            return len(s)
-        
-        l = 0
-        d = {}
-        res = 0
-        for r, val in enumerate(s):
-
-            if val in d and d[val] >= l:
-                l = d[val] + 1
-
-            d[val] = r
-
-            res = max(res, r - l + 1)
-        return res
-
-# s="abcabcbb"
-# s="pwwkew"
-s="dvdf"
-
-get = Solution()
-print(get.lengthOfLongestSubstring(s=s))
+from langchain.agents import create_agent
+from langchain_core.tools import tool
